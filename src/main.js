aa103d4a111674c5289dc19d353001bb3b04687e
shideshan <307042873@qq.com>
2019-01-03 11:45:46 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastClick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
+/* ES6中键和值一样时，写一个就可以，比如下面的router等价于router:router */
 new Vue({
   el: '#app',
   router,
