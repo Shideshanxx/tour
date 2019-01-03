4511951cb8b42b62d9ce14d1726440e743f7d79b
shideshan <307042873@qq.com>
2019-01-03 17:13:58 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,78 @@
+<template>
+    <div>
+        <div class="title">热销推荐</div>
+        <ul>
+            <!--加入1像素底边框，只需加入boder-bottom类，因为styles中加入了1像素解决方案库-->
+            <li class="item boder-bottom" v-for="item of recommendList" :key="item.id">
+                <img class="item-img" :src="item.imgUrl" />
+                <div class="item-info">
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.desc}}</p>
+                    <button class="item-button">查看详情</button>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeRecommend',
+    data () {
+        return {
+            recommendList: [{
+                id: '0001',
+                imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_140x140_73fda71d.jpg',
+                title: '大连圣亚海洋世界,大连圣亚海洋世界,大连圣亚海洋世界',
+                desc: '浪漫大连首站，浪漫的海洋主题乐园,浪漫大连首站，浪漫的海洋主题乐园'
+            },{
+                id: '0002',
+                imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_140x140_73fda71d.jpg',
+                title: '大连圣亚海洋世界,大连圣亚海洋世界,大连圣亚海洋世界',
+                desc: '浪漫大连首站，浪漫的海洋主题乐园,浪漫大连首站，浪漫的海洋主题乐园'
+            },{
+                id: '0003',
+                imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_140x140_73fda71d.jpg',
+                title: '大连圣亚海洋世界,大连圣亚海洋世界,大连圣亚海洋世界',
+                desc: '浪漫大连首站，浪漫的海洋主题乐园,浪漫大连首站，浪漫的海洋主题乐园'
+            }]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/mixins.styl'
+    .title
+        margin-top: .2rem
+        line-height: .8rem
+        background #eee
+        text-indent: .2rem
+    .item
+        overflow: hidden
+        height: 1.9rem
+        display: flex
+        .item-img
+            width: 1.7rem
+            height: 1.7rem
+            padding: .1rem
+        .item-info
+            flex: 1
+            padding: .1rem
+            min-width: 0   //必须加这一条下面的ellipsis()效果才不会出bug
+            .item-title
+                line-height: .54rem
+                font-size: .32rem
+                ellipsis()
+            .item-desc
+                line-height: .4rem
+                color: #ccc
+                ellipsis()
+            .item-button
+                line-height: .44rem
+                margin-top: .16rem
+                background #d66317
+                padding: 0 .2rem
+                border-radius: .06rem
+                color: #fff
+</style>
