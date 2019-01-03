0f90fa325a8cf5bc1eb37f9287bdf068af9f9d63
shideshan <307042873@qq.com>
2019-01-03 22:30:10 +0800

    perfomance

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,16 @@
 <template>
     <ul class="list">
-        <li class="item" v-for="(item, key) of cities" :key="key">{{key}}</li>
+        <li class="item" 
+        v-for="item of letters" 
+        :key="item"
+        :ref="item"
+        @touchstart="handleTouchStart"
+        @touchmove="handleTouchMove"
+        @touchend="handleTouchEnd"
+        @click="handleLetterClick"
+        >
+            {{item}}
+        </li>
     </ul>
 </template>
 
@@ -9,6 +19,56 @@ export default {
     name: 'CityAlphabet',
     props: {
         cities: Object
+    },
+    computed: {
+        letters () {
+            const letters = []
+            for (let i in this.cities) {
+                letters.push(i)
+            }
+            return letters
+        }
+    },
+    data () {
+        return {
+            touchStatus: false,
+            startY: 0,
+            timer: null
+        }
+    },
+    //刚开始cities为空，ajax请求之后，updated钩子就会被执行，
+    //下面是性能优化的一步，单独抽出来绑定到this.startY，以免重复计算startY
+    updated () {
+        this.startY = this.$refs['A'][0].offsetTop
+    },
+    methods: {
+        handleLetterClick (e) {
+            this.$emit('change',e.target.innerText)
+        },
+        handleTouchStart () {
+            this.touchStatus = true
+        },
+        handleTouchMove (e) {
+            //获取touch时的index，emit出去change事件和当前index值，然后change事件被监听，左侧内容跟随变动
+            if (this.touchStatus) {
+                //emit出去change事件有16ms的延迟，如果在延迟之内又触发该事件，
+                //则上一次未执行的事件将被清除；通过这种函数节流的方式，大大提升性能
+                if (this.timer) {
+                    clearTimeout(this.timer)
+                }
+                this.timer = setTimeout(() => {
+                    const touchY = e.touches[0].clientY - 79
+                    const index = Math.floor((touchY - this.startY) / 20)
+                    if (index >= 0 && index < this.letters.length) {
+                        this.$emit('change',this.letters[index])
+                    }
+                },16)
+                
+            }
+        },
+        handleTouchEnd () {
+            this.touchStatus = false
+        }
     }
 }
 </script>
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -17,7 +17,11 @@
                     </div>
                 </div>
             </div>
-            <div class="area" v-for="(item,key) of cities" :key="key">
+            <div class="area" 
+            v-for="(item,key) of cities" 
+            :key="key"
+            :ref="key"
+            >
                 <div class="title border-topbottom">{{key}}</div>
                 <div class="item-list">
                     <div class="item border-bottom" 
@@ -36,10 +40,20 @@ export default {
     name: 'CityList',
     props: {
         hot: Array,
-        cities: Object
+        cities: Object,
+        letter: String
     },
     mounted () {
         this.scroll = new Bscroll(this.$refs.wrapper)
+    },
+    watch: {
+        letter () {
+            if (this.letter) {
+                //用element缓存ref接受的循环出来的dom元素，scrollToElement是scroll自带的方法
+                const element = this.$refs[this.letter][0]
+                this.scroll.scrollToElement(element)
+            }
+        }
     }
 }
 </script>
